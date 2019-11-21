ade55a3a44526950ac93f930917bed9b7898fd11
Kevin McDermott <kmcdermo@redhat.com>
2019-11-21 11:12:29 +0000

    Update the script to use oc and set permissions appropriately.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-kubectl apply -f https://github.com/tektoncd/pipeline/releases/download/v0.8.0/release.yaml
-kubectl apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
-kubectl apply -f quayio-secret.yaml
-kubectl apply -f serviceaccount
-kubectl apply -f templatesandbindings
-kubectl apply -f eventlisteners/eventlistener.yaml
-kubectl apply -f buildpipeline
-echo "kubectl port-forward svc/el-demo-event-listener 8080"
+oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.8.0/release.yaml
+oc apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
+oc apply -f quayio-secret.yaml
+oc apply -f serviceaccount
+oc adm policy add-scc-to-user privileged -z demo-sa
+oc adm policy add-role-to-user edit -z demo-sa
+oc apply -f templatesandbindings
+oc apply -f eventlisteners/eventlistener.yaml
+oc apply -f buildpipeline
+echo "oc port-forward svc/el-demo-event-listener 8080"
 echo "./demo.sh"
 echo "tkn pipelinerun list"
